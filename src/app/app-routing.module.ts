26c99e446214b4eb31d75a37639ced477d42642d
Kingsley Etoka <etokakingsley@gmail.com>
2022-06-16 14:27:42 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './pages/auth/create-account/create-account.component';
+import { ForgotPasswordComponent } from './pages/auth/forgot-password/forgot-password.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
+import { AddNewComponent } from './pages/dashboard/add-new/add-new.component';
+import { HomeComponent } from './pages/dashboard/home/home.component';
+import { MainComponent } from './pages/dashboard/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'create-account', component: CreateAccountComponent},
+  {path:'forgot-password', component: ForgotPasswordComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
+  {path:'dashboard', component: MainComponent, children:[
+    {path: '', component:HomeComponent },
+    {path:'add', component:  AddNewComponent},
+  ]},
+  {path:'**', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { CreateAccountComponent } from './pages/auth/create-account/create-account.component';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './pages/auth/forgot-password/forgot-password.component';
+import { MainComponent } from './pages/dashboard/main/main.component';
+import { HomeComponent } from './pages/dashboard/home/home.component';
+import { AddNewComponent } from './pages/dashboard/add-new/add-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    ResetPasswordComponent,
+    ForgotPasswordComponent,
+    MainComponent,
+    HomeComponent,
+    AddNewComponent
   ],
   imports: [
     BrowserModule,
